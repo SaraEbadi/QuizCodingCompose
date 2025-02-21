5164abf0eea9a51e58633170dad7274e3f667e8f
sarahebadi <ebadi.sara227@gmai.com>
2025-02-21 19:20:29 +0330

    Implemented MainScreen ui.

---
+++ app/src/main/java/com/saraebadi/quizcodingcompose/presentation/main_menu/MainScreen.kt
@@ -0,0 +1,86 @@
+package com.saraebadi.quizcodingcompose.presentation.main_menu
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.saraebadi.quizcodingcompose.R
+import com.saraebadi.quizcodingcompose.ui.theme.DarkBlue
+import com.saraebadi.quizcodingcompose.ui.theme.LightBlue
+
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .background(DarkBlue)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.check_24),
+            fontSize = 72.sp,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = stringResource(R.string.quiz),
+            fontSize = 32.sp,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(R.string.high_score),
+            fontSize = 14.sp,
+            color = Color.White
+        )
+        Text(
+            text = stringResource(R.string.Score, 500),
+            fontSize = 14.sp,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp),
+            colors =ButtonDefaults.buttonColors(
+                containerColor = LightBlue
+            ),
+            shape = RoundedCornerShape(4.dp),
+            content = {
+                Text(text = stringResource(R.string.start),
+                    fontSize = 14.sp)
+            },
+            onClick = {
+
+            }
+        )
+
+    }
+}
+
+@Preview
+@Composable
+private fun MainScreenPreview() {
+    MainScreen()
+}
--- app/src/main/java/com/saraebadi/quizcodingcompose/ui/theme/Color.kt
+++ app/src/main/java/com/saraebadi/quizcodingcompose/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.saraebadi.quizcodingcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val DarkBlue = Color(0xFF005ea8)
+val LightBlue = Color(0xFF3894DC)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
