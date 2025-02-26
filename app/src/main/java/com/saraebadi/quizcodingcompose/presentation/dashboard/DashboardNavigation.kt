e79ea6f54f6a3c15962fcbd4ee3f09f97670d8d5
sarahebadi <ebadi.sara227@gmai.com>
2025-02-26 20:55:52 +0330

    Add state and collect it.

--- app/src/main/java/com/saraebadi/quizcodingcompose/presentation/dashboard/DashboardNavigation.kt
+++ app/src/main/java/com/saraebadi/quizcodingcompose/presentation/dashboard/DashboardNavigation.kt
@@ -1,6 +1,8 @@
 package com.saraebadi.quizcodingcompose.presentation.dashboard
 
+import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.saraebadi.quizcodingcompose.presentation.quiz.QuizRoute
@@ -14,9 +16,9 @@ fun NavGraphBuilder.dashboardScreen(
     ) {
     composable<DashboardRoute> {
         val viewModel: DashboardViewModel = hiltViewModel()
-        val state by viewModel.uiState.colle
+        val state by viewModel.uiState.collectAsStateWithLifecycle()
         DashboardScreen(
-            state = ,
+            state = state,
             effects = viewModel.effects,
             navigateToQuizScreen = navigateToQuizScreen,
             dashboardActions = viewModel
--- app/src/main/java/com/saraebadi/quizcodingcompose/presentation/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/saraebadi/quizcodingcompose/presentation/dashboard/DashboardViewModel.kt
@@ -6,7 +6,10 @@ import com.saraebadi.quizcodingcompose.presentation.quiz.QuizRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,6 +17,13 @@ import javax.inject.Inject
 class DashboardViewModel @Inject constructor(): ViewModel(), DashboardActions {
 
     private val _uiState = MutableStateFlow(DashboardUiState())
+    val uiState = _uiState.onStart {
+
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000L),
+        DashboardUiState()
+    )
 
     private val _effect = Channel<DashboardEffects>()
     val effects = _effect.receiveAsFlow()
