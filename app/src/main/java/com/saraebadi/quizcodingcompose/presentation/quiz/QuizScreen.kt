b34d122f7cd12f5530f34ff277d9162760c761b5
Sara <ebadi.sara227@gmail.com>
2025-02-27 19:50:23 +0330

    WIP-Implementing QuizScreen.

--- app/src/main/java/com/saraebadi/quizcodingcompose/presentation/quiz/QuizScreen.kt
+++ app/src/main/java/com/saraebadi/quizcodingcompose/presentation/quiz/QuizScreen.kt
@@ -1,17 +1,69 @@
 package com.saraebadi.quizcodingcompose.presentation.quiz
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.saraebadi.quizcodingcompose.R
+import com.saraebadi.quizcodingcompose.ui.theme.DarkBlue
 import com.saraebadi.quizcodingcompose.ui.theme.QuizCodingComposeTheme
 
 @Composable
 fun QuizScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.background(Color.Green)) {
+    val lists = listOf(1,2,3,4)
+    Column(modifier = modifier.background(DarkBlue).fillMaxSize()) {
+        Spacer(Modifier.height(40.dp))
+        Row(Modifier.fillMaxWidth().background(Color.White)) {
+            Text(
+                text = "Frage 2/3",
+                fontSize = 14.sp,
+                color = DarkBlue
+            )
+        }
+        Spacer(Modifier.height(40.dp))
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            )
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "score of this Q", color = Color.Green)
+                Image(painter = painterResource(R.drawable.ic_launcher_background), contentDescription = "")
+                Spacer(Modifier.height(16.dp))
+                Text(text = "question")
+            }
+        }
 
+        LazyColumn {
+            items(lists.size, key = { item -> item}) {
+
+            }
+        }
     }
 }
 
