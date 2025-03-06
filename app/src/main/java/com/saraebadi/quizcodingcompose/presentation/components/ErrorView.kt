df1400e6f684731118a2649a5addf5200b683462
Sara <ebadi.sara227@gmail.com>
2025-03-06 22:32:26 +0330

    Added Error view.

---
+++ app/src/main/java/com/saraebadi/quizcodingcompose/presentation/components/ErrorView.kt
@@ -0,0 +1,32 @@
+package com.saraebadi.quizcodingcompose.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.saraebadi.quizcodingcompose.R
+
+@Composable
+fun ErrorView(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(R.drawable.ic_broken_image),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
