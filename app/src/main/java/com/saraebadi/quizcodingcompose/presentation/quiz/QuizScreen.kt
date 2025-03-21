329b0c67934a252f510ef3987d4326e50eb90c1a
Sara <ebadi.sara227@gmail.com>
2025-03-21 23:36:33 +0330

    Added coil imageLoading basic.

--- app/src/main/java/com/saraebadi/quizcodingcompose/presentation/quiz/QuizScreen.kt
+++ app/src/main/java/com/saraebadi/quizcodingcompose/presentation/quiz/QuizScreen.kt
@@ -1,6 +1,5 @@
 package com.saraebadi.quizcodingcompose.presentation.quiz
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -22,13 +22,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.saraebadi.quizcodingcompose.R
+import coil3.compose.AsyncImage
 import com.saraebadi.quizcodingcompose.ui.theme.DarkBlue
 import com.saraebadi.quizcodingcompose.ui.theme.Gray100
 import com.saraebadi.quizcodingcompose.ui.theme.QuizCodingComposeTheme
@@ -70,7 +70,14 @@ fun QuizScreen(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(text = "${state.quiz?.score.toString()} Punkte", color = Color.Green)
-                Image(painter = painterResource(R.drawable.ic_launcher_background), contentDescription = "")
+                AsyncImage(
+                    model = state.quiz?.questionImageUrl,
+                    contentDescription = "quiz-image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .size(200.dp)
+                )
                 Spacer(Modifier.height(16.dp))
                 Text(
                     text = state.quiz?.question ?: "",
