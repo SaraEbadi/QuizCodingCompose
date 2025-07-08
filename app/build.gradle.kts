fd295596f711d8d4571a5934fbd5ecc1d1efd444
sara <ebadi.sara227@gmail.com>
2025-07-08 15:54:45 +0330

    Upgraded dependencies and commented out VisualMemoryTestScreen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.saraebadi.quizcodingcompose"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.saraebadi.quizcodingcompose"
